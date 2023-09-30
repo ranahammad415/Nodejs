54d65134f7057ff72f7396bde2f96608759653f0
engineeringdloyal <enghammad@dloyal.com>
2023-09-30 21:42:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 //const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 const app = express();
 const PORT = 4000;
