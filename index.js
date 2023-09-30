f5cf578bbe9196b5e387d37263981c21b3678a32
engineeringdloyal <enghammad@dloyal.com>
2023-09-30 17:29:09 +0500

    push

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
-const puppeteer = require('puppeteer');
+
+const chromium = require('chrome-aws-lambda');
+
 
 const app = express();
 const port = 4000;
@@ -24,7 +26,13 @@ app.get('/scrape', async (req, res) => {
   try {
     const { hash } = req.query;
 
-    const browser = await puppeteer.launch();
+    const browser = await chromium.puppeteer.launch({
+      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    })
     const page = await browser.newPage();
 
     // Modify the URL to include query parameters
